2be566c8e9b924c1d3030c2007b18ed9158e5f77
Louis Timti <louistimti1@gmail.com>
2022-06-12 11:23:05 +0100

    First try
    First try at exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+	let word = '';
+	if (num < 0) return 'ERROR';
+	for (let i = 0; i < num; i++) {
+		word += string;
+	}
+	return word;
 };
+
+console.log(repeatString('hey', 2));
 
 // Do not edit below this line
 module.exports = repeatString;
